ff8c69f867b181a59188334fa86badff873e72be
Matt Wynne <matt@cucumber.io>
2021-10-20 13:03:45 -0700

    Allow running getTeamId as a script

--- getTeamId.ts
+++ getTeamId.ts
@@ -1,10 +1,20 @@
 import { Octokit } from "@octokit/rest";
+import { getTeam } from "@pulumi/github";
 export const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+const org = process.env.GITHUB_OWNER || "unknown";
 
 export async function getTeamId(name: string) {
   const response = await octokit.request("GET /orgs/{org}/teams/{team_slug}", {
-    org: "cucumber",
+    org,
     team_slug: name,
   });
   return String(response.data.id);
+}
+
+if (process.argv.length > 2) {
+  const teamName = process.argv[2];
+  getTeamId(teamName)
+    .then((id) => console.log(id))
+    .catch((error) => console.error(`${error.message}: ${error.request.url}`));
 }
