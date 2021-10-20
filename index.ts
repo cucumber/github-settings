0a3ed5e612dd4a355b028e851b5bc8c6ff491a03
Matt Wynne <matt@cucumber.io>
2021-10-20 08:29:28 -0700

    Play with another way to factor the code

--- index.ts
+++ index.ts
@@ -1,41 +1,64 @@
 import * as github from "@pulumi/github";
 import { getTeamId } from "./getTeamId";
 
-const repoName = "awesome-cucumber";
-
 interface TeamRepository {
   repoName: string;
   teamName: string;
   permission: "pull" | "triage" | "push" | "maintain" | "admin";
 }
 
-const teamName = "release";
-const permission = "maintain";
-function setTeam({ repoName, teamName, permission }: TeamRepository) {
+function setTeam(
+  result: any,
+  { repoName, teamName, permission }: TeamRepository
+) {
   const name = ["team", teamName, "repo", repoName].join(".");
   const teamRepo = new github.TeamRepository(name, {
     repository: repoName,
     teamId: getTeamId(teamName),
     permission,
   });
+  if (!result.teams) {
+    result.teams = {};
+  }
+  result.teams[[repoName, ":", teamName].join("")] = teamRepo.id;
 }
 
-setTeam({
-  repoName: "awesome-cucumber",
-  teamName: "release",
-  permission: "maintain",
-});
-setTeam({
-  repoName: "awesome-cucumber",
-  teamName: "committers",
-  permission: "push",
-});
-setTeam({
+type Permission = "pull" | "triage" | "push" | "maintain" | "admin";
+
+interface RepoSettings {
+  (repoName: string, result: any): void;
+}
+
+interface AppliesRepoSettings {
+  (...settings: RepoSettings[]): any;
+}
+
+function repo(repoName: string): AppliesRepoSettings {
+  const result = {};
+  return (...settings) => {
+    settings.map((setting) => setting(repoName, result));
+    return result;
+  };
+}
+
+function manageAccessForTeam(
+  teamName: string,
+  permission: Permission
+): RepoSettings {
+  return (repoName, result) =>
+    setTeam(result, { repoName, teamName, permission });
+}
+
+export const output = repo("awesome-cucumber")(
+  manageAccessForTeam("release", "maintain"),
+  manageAccessForTeam("committers", "push")
+);
+setTeam(output, {
   repoName: "create-meta",
   teamName: "release",
   permission: "maintain",
 });
-setTeam({
+setTeam(output, {
   repoName: "github-settings",
   teamName: "release",
   permission: "maintain",
